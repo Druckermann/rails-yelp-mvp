785f484cfb951cd0fe0a602633a99eb9c23e8598
Allan Turton <forshopping50@hotmail.com>
2023-03-25 12:10:27 +1000

    Same as before really - next stop: validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,14 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+
+
 end
+
+
+
+# A restaurant must have a name, an address and a category.
+# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# When a restaurant is destroyed, all of its reviews must be destroyed as well.
+
+# DONE - A review must belong to a restaurant.
