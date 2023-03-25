e0db0be448a3ccbc7a7d281d976c1a920ec1ae7f
Allan Turton <forshopping50@hotmail.com>
2023-03-25 13:40:28 +1000

    Model validations done. Seed is next

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,13 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
-
-
-
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: {in: ["chinese", "italian", "japanese", "french", "belgian"]}
 end
 
 
 
-# A restaurant must have a name, an address and a category.
-# A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
+# DONE - A restaurant must have a name, an address and a category.
+# DONE - A restaurant’s category must belong to this fixed list: ["chinese", "italian", "japanese", "french", "belgian"].
 # When a restaurant is destroyed, all of its reviews must be destroyed as well.
 
 # DONE - A review must belong to a restaurant.
